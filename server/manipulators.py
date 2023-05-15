abc6a44d645e2e21be4125e2ad3bb70bb653c339
Hryhorii Biloshenko <hryhorii.biloshenko@gmail.com>
2023-05-15 15:58:55 +0200

    add default manipulators class

--- server/manipulators.py
+++ server/manipulators.py
@@ -1,9 +1,9 @@
 from typing import Protocol, Unpack
 
-from server.command_handlers.command_creator import CommandCreator, CommandCreatorKwargs
-from server.command_handlers.command_matcher import CommandMatcher, CommandMatcherKwargs
-from server.socket_handlers.read_handler import ReadHandler, ReadHandlerKwargs
-from server.socket_handlers.write_handler import WriteHandler, WriteHandlerKwargs
+from server.command_handlers.command_creator import CommandCreator, CommandCreatorKwargs, DefaultCommandCreator
+from server.command_handlers.command_matcher import CommandMatcher, CommandMatcherKwargs, DefaultCommandMatcher
+from server.socket_handlers.read_handler import ReadHandler, ReadHandlerKwargs, Split2BytesReadHandler
+from server.socket_handlers.write_handler import DefaultWriteHandler, WriteHandler, WriteHandlerKwargs
 
 
 class Manipulators(Protocol):
@@ -18,3 +18,17 @@ class Manipulators(Protocol):
 
     def get_writer(self, **kwargs: Unpack[WriteHandlerKwargs]) -> WriteHandler:
         ...
+
+
+class DefaultManipulators:
+    def get_matcher(self, **kwargs: Unpack[CommandMatcherKwargs]) -> CommandMatcher:
+        return DefaultCommandMatcher(**kwargs)
+
+    def get_creator(self, **kwargs: Unpack[CommandCreatorKwargs]) -> CommandCreator:
+        return DefaultCommandCreator(**kwargs)
+
+    def get_reader(self, **kwargs: Unpack[ReadHandlerKwargs]) -> ReadHandler:
+        return Split2BytesReadHandler(**kwargs)
+
+    def get_writer(self, **kwargs: Unpack[WriteHandlerKwargs]) -> WriteHandler:
+        return DefaultWriteHandler(**kwargs)
