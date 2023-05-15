5344175632caca9833b43a979f2c86ad95cb6fd8
Hryhorii Biloshenko <hryhorii.biloshenko@gmail.com>
2023-05-15 15:14:33 +0200

    add socket reading handler for 2 bytes message separator

--- server/socket_handlers/read_handler.py
+++ server/socket_handlers/read_handler.py
@@ -1,12 +1,17 @@
 import asyncio
 from abc import ABC, abstractmethod
+from collections import deque
 from dataclasses import KW_ONLY, dataclass, field
+from io import BytesIO
 from logging import Logger
 from typing import Required, TypedDict
 
-from common.config import TIMEOUT
+from common.config import CMD_POSTFIX_B, TIMEOUT
+from common.result import Err, Ok
 from server.command_handlers.command_matcher import CommandMatcher
+from server.exceptions import ServerTimeoutError
 from server.server_result import ServerResult
+from server.socket_handlers.types import SeparatorChecker
 
 from . import logger as LOGGER_BASE
 
@@ -32,3 +37,92 @@ class ReadHandler(ABC):
     @abstractmethod
     async def read(self, max_len: int, *, timeout: int = TIMEOUT) -> ServerResult[bytes]:
         pass
+
+
+class Split2BytesReadHandler(ReadHandler):
+    async def read(self, max_len: int, *, timeout: int = TIMEOUT) -> ServerResult[bytes]:
+        sep = CMD_POSTFIX_B
+        self.logger.debug("started read")
+        if len(sep) != 2:
+            raise ValueError("2 bytes reader takes only 2 bytes long message separator")
+
+        msg = self._get_from_queue(sep)  # get previously received messages if any
+        if msg[-2:] == sep:  # if received message was already fully read
+            return Ok(msg)
+
+        msg_stream = BytesIO()
+        msg_stream.write(msg)
+
+        self.logger.debug(f"started with data: {msg}")
+        length = len(msg)  # length of read data
+
+        separator_spliter = self._get_separator_spliter(sep, bool(msg and msg[-1] == sep[0]))
+
+        while length < max_len:
+            self.logger.debug("try read")
+            try:
+                chunk = await asyncio.wait_for(self.reader.read(self._chunk_size), timeout=timeout)
+            except (TimeoutError, ConnectionResetError):
+                return Err(ServerTimeoutError(f"Timeout exceeded: {timeout=}"))
+            if chunk == b"":
+                return Err(ServerTimeoutError(f"Connection is probably closed by the other side"))
+
+            self.logger.debug(f"recieved data: {chunk}")
+
+            part_of_current, part_of_next = separator_spliter(chunk)
+            length += len(part_of_current)
+            msg_stream.write(part_of_current)
+
+            if part_of_next:
+                self._msg_queue.append(part_of_next)
+
+            if part_of_next is not None:
+                break
+
+        self.logger.debug(f"return value: {msg_stream.getvalue()}")
+        return Ok(msg_stream.getvalue()[:max_len])
+
+    def _split_by_separator(self, chunk: bytes, sep: bytes) -> tuple[bytes, bytes]:
+        split_index = chunk.index(sep) + len(sep)
+        return chunk[:split_index], chunk[split_index:]
+
+    def _get_from_queue(self, sep: bytes) -> bytes:
+        """Gets message from queue until separator or until the queue ends"""
+        msg_stream = BytesIO()
+        separator_spliter = self._get_separator_spliter(sep)
+        while self._msg_queue:
+            next_chunk = self._msg_queue[0]
+            part_of_current, part_of_next = separator_spliter(next_chunk)
+            msg_stream.write(part_of_current)
+            if part_of_next is not None:
+                if part_of_next:  # separator end is in between of the chunk
+                    self._msg_queue[0] = part_of_next
+                else:  # separator end in the end of the chunk
+                    self._msg_queue.popleft()
+                return msg_stream.getvalue()
+            else:
+                self._msg_queue.popleft()
+
+        return msg_stream.getvalue()
+
+    def _get_separator_spliter(self, sep: bytes, found_half: bool = False) -> SeparatorChecker:
+        """
+        Returns a callable which takes chunk and splits it by separator.
+        It returns tuple[bytes, bytes | None].
+        Second element is None if current message didn't end.
+        """
+
+        def _inner(chunk: bytes) -> tuple[bytes, bytes | None]:
+            nonlocal found_half
+            if found_half and chunk and chunk[0] == sep[1]:  # if sep was on the start and the end of 2 chunks
+                return chunk[0:1], chunk[1:]  # [0:1] to get first byte as bytes instead of int
+            elif sep in chunk:
+                return self._split_by_separator(chunk, sep)
+            elif chunk and chunk[-1] == sep[0]:  # if chunk ends with first symbol of sep
+                found_half = True
+            else:
+                found_half = False
+
+            return chunk, None
+
+        return _inner
