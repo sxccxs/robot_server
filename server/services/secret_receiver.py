d1c342419a0c020fd2578d90a70002f2341d53f4
Hryhorii Biloshenko <hryhorii.biloshenko@gmail.com>
2023-05-16 18:13:55 +0200

    add default secret message receiver

--- server/services/secret_receiver.py
+++ server/services/secret_receiver.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from common.commands import ClientCommand, ServerCommand
+from common.result import Err, Ok
 from server.server_result import NoneServerResult
 from server.services.base_service import BaseService, BaseServiceKwargs
 
@@ -13,3 +15,22 @@ class SecretReceiver(BaseService, ABC):
     async def receive(self) -> NoneServerResult:
         "Receives secret message from client"
         ...
+
+
+class DefaultSecretReceiver(SecretReceiver):
+    async def receive(self) -> NoneServerResult:
+        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_PICK_UP))
+        match await self.reader.read(ClientCommand.CLIENT_MESSAGE.max_len_postfix):
+            case Err() as err:
+                self.logger.error(f"Error in secret receiving: {err=}")
+                return err
+            case Ok(data):
+                pass
+        match self.matcher.match(ClientCommand.CLIENT_MESSAGE, data):
+            case Err() as err:
+                self.logger.error(f"Error in secret receiving: {err=}")
+                return err
+            case Ok():
+                await self.writer.write(self.creator.create_message(ServerCommand.SERVER_LOGOUT))
+                self.logger.info("Logout success")
+                return Ok(None)
