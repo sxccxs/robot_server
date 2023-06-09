4115b1ed12220f22d4ab55f0593ab2c67dee7118
Hryhorii Biloshenko <hryhorii.biloshenko@gmail.com>
2023-06-09 19:37:57 +0200

    add some methods to mover base class

--- server/services/mover.py
+++ server/services/mover.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from functools import partial
 
+from typing_extensions import override
+
 from common.commands import ClientCommand, ServerCommand
 from common.data_classes import Axis, Coords, Orientation, Side
 from common.result import Err, Ok
@@ -18,12 +20,80 @@ class Mover(BaseService, ABC):
     async def move_to_start(self) -> NoneServerResult:
         pass
 
+    async def _rotate_to(self, orient: Orientation, to_side: Side) -> None:
+        if orient.side == to_side:
+            return
+
+        if (orient.side - to_side) % 4 == 1:
+            await self._turn_left(orient)
+            return
+
+        count = 1
+        if (orient.side - to_side) % 4 == 2:
+            count = 2
+        for _ in range(count):
+            await self._turn_right(orient)
+
+    async def _get_orientation(self) -> Orientation | None:
+        """Determines orientation of the robot by making 2 moves forward.
+        If obstacle was encountered, turns left and moves again.
+
+        Returns:
+            Orientation | None: If after first move position is (0,0), returns None,
+            else determined orientation object.
+        """
+        pos = await self._make_move()
+        if pos == Coords(0, 0):
+            return None
+        new_pos = await self._make_move()
+        if pos == new_pos:  # if found obstacle, turn left and move again
+            await self._turn_left()
+            new_pos = await self._make_move()
+
+        side = Side.determine_side(pos, new_pos)
+        return Orientation(new_pos, side)
+
+    async def _make_move(self) -> Coords:
+        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_MOVE))
+        return await self._get_ok_response()
+
+    async def _turn_right(self, orient: Orientation | None = None) -> Coords:
+        if orient is not None:
+            orient.turn_right()
+        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_TURN_RIGHT))
+        return await self._get_ok_response()
+
+    async def _turn_left(self, orient: Orientation | None = None) -> Coords:
+        if orient is not None:
+            orient.turn_left()
+        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_TURN_LEFT))
+        return await self._get_ok_response()
+
+    async def _get_ok_response(self) -> Coords:
+        match await self.reader.read(ClientCommand.CLIENT_OK.max_len_postfix):
+            case Err(err):
+                raise err
+            case Ok(value):
+                data = value
+        match self.matcher.match(ClientCommand.CLIENT_OK, data):
+            case Err(err):
+                raise err
+            case Ok(data):
+                return data
+
 
 class DefaultMover(Mover):
+    @override
     async def move_to_start(self) -> NoneServerResult:
         self.logger.debug("Started mover")
         try:
-            orientation = await self._get_orientation()
+            match await self._get_orientation():
+                case None:
+                    self.logger.info("Moved to (0,0).")
+                    return Ok(None)
+                case Orientation() as orientation:
+                    pass
+
             self.logger.info(
                 f"Starting position: {orientation.coords}, starting orientation: {orientation.side.name}"
             )
@@ -36,17 +106,18 @@ class DefaultMover(Mover):
         return Ok(None)
 
     async def _move_to_0(self, orient: Orientation, axis: Axis) -> None:
-        """Moves to 0 by the specified axis
+        """Moves to 0 by the specified axis.
 
         Args:
-            orient (Orientation): orientation object determening current position
-            axis (Axis): axis to move along
+            orient (Orientation): orientation object determening current position.
+            axis (Axis): axis to move along.
         """
         self.logger.info(f"Started moving to 0 by axis {axis.name}")
         if axis == Axis.X:
             move_side = Side.LEFT if orient.coords.x > 0 else Side.RIGHT
         else:
             move_side = Side.DOWN if orient.coords.y > 0 else Side.UP
+
         await self._rotate_to(orient, move_side)
 
         condition = (lambda: orient.coords.x != 0) if axis == Axis.X else (lambda: orient.coords.y != 0)
@@ -80,47 +151,6 @@ class DefaultMover(Mover):
         await self._make_move()
 
         return await self._turn_right()
-
-    async def _rotate_to(self, orient: Orientation, to_side: Side) -> None:
-        while orient.side != to_side:
-            await self._turn_right(orient)
-
-    async def _get_orientation(self) -> Orientation:
-        pos = await self._make_move()
-        new_pos = await self._make_move()
-        if pos == new_pos:  # if found obstacle, turn left and move again
-            await self._turn_left()
-            new_pos = await self._make_move()
-
-        side = self._determine_side(pos, new_pos)
-        return Orientation(new_pos, side)
-
-    async def _make_move(self) -> Coords:
-        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_MOVE))
-        return await self._get_ok_response()
-
-    async def _turn_right(self, orient: Orientation | None = None) -> Coords:
-        if orient is not None:
-            orient.turn_right()
-        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_TURN_RIGHT))
-        return await self._get_ok_response()
-
-    async def _turn_left(self, orient: Orientation | None = None) -> Coords:
-        if orient is not None:
-            orient.turn_left()
-        await self.writer.write(self.creator.create_message(ServerCommand.SERVER_TURN_LEFT))
-        return await self._get_ok_response()
-
-    async def _get_ok_response(self) -> Coords:
-        match await self.reader.read(ClientCommand.CLIENT_OK.max_len_postfix):
-            case Err(err):
-                raise err
-            case Ok(data):
-                match self.matcher.match(ClientCommand.CLIENT_OK, data):
-                    case Err(err):
-                        raise err
-                    case Ok(data):
-                        return data
 
 
 
